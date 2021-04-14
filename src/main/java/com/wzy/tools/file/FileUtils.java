5f6349ca49f45e3389cdef443747c8b5335fb01a
Wzy <123@qq.com>
2021-04-14 16:20:17 +0800

    1.新增批量删除匹配到的文件夹功能

--- src/main/java/com/wzy/tools/file/FileUtils.java
+++ src/main/java/com/wzy/tools/file/FileUtils.java
@@ -104,6 +104,50 @@ public class FileUtils {
         return paths;
     }
 
+    /**
+     * 通过文件夹名称获取目录下匹配到的文件夹
+     * @param path 文件路劲
+     * @param express 表达式
+     * @return
+     */
+    public static List<String> matchDirsByName(String path, String express){
+        return matchDirs(path, express, false);
+    }
+
+    /**
+     * 获取目录下匹配到的文件夹
+     * @param path 文件路劲
+     * @param express 表达式
+     * @param isRegular 是否支持正则
+     * @return
+     */
+    public static List<String> matchDirs(String path, String express, boolean isRegular) {
+        if (!checkPath(path)) {
+            return null;
+        }
+        List<String> paths = new ArrayList<>();
+        File file = new File(path);
+
+        if (file.isDirectory()) {
+            // 匹配到对应文件夹添加,否则继续递归匹配
+            if (doMatch(file, express, isRegular)) {
+                paths.add(file.getPath());
+            } else {
+                File[] subFiles = file.listFiles();
+                if (null != subFiles || subFiles.length == 0 ) {
+                    for (File subFile : subFiles) {
+                        List<String> subPaths = matchDirs(subFile.getPath(), express, isRegular);
+                        if (subPaths == null || subPaths.isEmpty()) {
+                            continue;
+                        }
+                        paths.addAll(subPaths);
+                    }
+                }
+            }
+        }
+        return paths;
+    }
+
     private static boolean doMatch(File file, String express, boolean isRegular) {
         if (isRegular) {
             if (file.getName().matches(express)) {
@@ -126,6 +170,87 @@ public class FileUtils {
             return false;
         }
         return true;
+    }
+
+    /**
+     * 批量删除文件
+     * @param paths 需要删除的文件路径
+     * @return
+     */
+    public static void deleteFiles(List<String> paths) {
+        File file;
+        for(String path : paths) {
+            file = new File(path);
+            if (file.isDirectory()) {
+                deleteDirectory(path);
+            } else {
+                deleteFile(path);
+            }
+        }
+    }
+
+    /**
+     * 删除单个文件
+     * @param   sPath    被删除文件的文件名
+     * @return 单个文件删除成功返回true，否则返回false
+     */
+    public static boolean deleteFile(String sPath) {
+        boolean flag = false;
+        File file = new File(sPath);
+        // 路径为文件且不为空则进行删除
+        if (file.isFile() && file.exists()) {
+            file.delete();
+            flag = true;
+        }
+        return flag;
+    }
+
+    /**
+     * 删除目录（文件夹）以及目录下的文件
+     * @param   sPath 被删除目录的文件路径
+     * @return  目录删除成功返回true，否则返回false
+     */
+    public static boolean deleteDirectory(String sPath) {
+        //如果sPath不以文件分隔符结尾，自动添加文件分隔符
+        if (!sPath.endsWith(File.separator)) {
+            sPath = sPath + File.separator;
+        }
+        File dirFile = new File(sPath);
+        //如果dir对应的文件不存在，或者不是一个目录，则退出
+        if (!dirFile.exists() || !dirFile.isDirectory()) {
+            return false;
+        }
+        boolean flag = true;
+        //删除文件夹下的所有文件(包括子目录)
+        File[] files = dirFile.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            //删除子文件
+            if (files[i].isFile()) {
+                flag = deleteFile(files[i].getAbsolutePath());
+                if (!flag) break;
+            } //删除子目录
+            else {
+                flag = deleteDirectory(files[i].getAbsolutePath());
+                if (!flag) break;
+            }
+        }
+        if (!flag) return false;
+        //删除当前目录
+        if (dirFile.delete()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        String path = "E:\\wzy\\V2";
+        String dir = "target";
+        List<String> dirs = matchDirsByName(path, dir);
+        dirs.forEach(obj->{
+            System.out.println(obj + " ");
+        });
+        deleteFiles(dirs);
     }
 
 }
